457f9166777a3d80caa0ce84f5b4f065afb7571b
Anesh Soamanath Majalikar <64136492+AneshSM@users.noreply.github.com>
2023-03-08 18:32:28 +0530

    up

--- Web_Designs/Dashboards/responsive/index.js
+++ Web_Designs/Dashboards/responsive/index.js
@@ -25,6 +25,24 @@ function menu(status) {
     }
 }
 
-function navigate(a){
-    a.classList.add(" active");
+const show_card=document.querySelectorAll('.show_card');
+const bar_list=document.querySelectorAll(".bar_options");
+const head_title=document.getElementById('head_title');
+navigate();
+function navigate(title="Dashboard"){
+    head_title.innerHTML=title;
+    const opt=document.getElementById(title);
+    bar_list.forEach(element => {
+        element.classList.remove('active')
+    });
+    opt.classList.add('active');
+    if( (window.innerWidth || document.documentElement.clientWidth) <430) {
+        menu("close_menu");
+    }
+    show_card.forEach(element=>{
+        element.style.display="none";
+        console.log(element);
+    });
+    const card=document.getElementById(title+"_card");
+    card.style.display="flex";
 }
