5e702b4da813e4e471387385b90cbd1761de099d
Anesh Soamanath Majalikar <64136492+AneshSM@users.noreply.github.com>
2023-03-14 13:37:31 +0530

    update

--- Web_Designs/Dashboards/responsive/index.js
+++ Web_Designs/Dashboards/responsive/index.js
@@ -1,9 +1,4 @@
-const window_size = {
-    innerwidth: window.innerWidth,
-    innerhidth: window.innerHeight,
-    clientwidth: document.documentElement.clientWidth,
-    clientheight: document.documentElement.clientHeight,
-}
+
 const color = {
     color_60: "#1A1C22",
     color_30: "#282C35",
@@ -19,22 +14,42 @@ const side_bar_content = {
     logo: document.getElementById('logo'),
     content: document.getElementsByClassName('side_bar_content'),
     icon: document.getElementsByClassName('side_bar_icon'),
+    bar_option: document.querySelectorAll(".bar_options"),
 }
 //maain container
-const main_container={
+const container = {
+    main_container: document.getElementById('main_container'),
     header: document.getElementById('header'),
-    main_content: document.getElementById('main_content'), 
+    main_content: document.getElementById('main_content'),
+    card_dashboard: document.getElementById('Dashboard_card'),
+    container: document.getElementById('container'),
+    container_1: document.getElementById('container_1'),
+    container_1_1: document.getElementById('container_1_1'),
+}
+
+const card_container = {
+    kpi_container: document.getElementById('kpi_cards'),
+    kpi_cards: document.getElementsByClassName('kpi_card'),
+    line_chart: document.getElementById('line_chart'),
 }
 
 
 
+setInterval(() => {
+    if ((window.innerWidth || window.document.clientWidth) < 1301) {
+        container.container_1_1.insertBefore(card_container.kpi_container, card_container.line_chart);
+    }
+    else if ((window.innerWidth || window.document.clientWidth) > 1300) {
+        container.card_dashboard.insertBefore(card_container.kpi_container, container.container);
+    }
+}, 100);
 
-if((window_size.innerwidth || window_size.innerWidth)>768){
+
 menu();
 // side bar
 function menu(status = 'close_menu') {
     if (status === "close_menu") {
-        side_bar_content.side_bar.style.width = '70px';
+        side_bar_content.side_bar.style.width = '60px';
         setTimeout(() => {
             side_bar_content.open_menu.style.display = 'flex';
             side_bar_content.close_menu.style.display = 'none';
@@ -45,11 +60,31 @@ function menu(status = 'close_menu') {
                 side_bar_content.content[i].style.display = 'none';
             }
         }, 200);
+        setTimeout(() => {
+            for (let i = 0; i < side_bar_content.bar_option.length; i++) {
+                side_bar_content.bar_option[i].style.justifyContent = 'center';
+                side_bar_content.bar_option[i].style.paddingLeft = '0%';
+            }
+        }, 550);
+        setTimeout(() => {
+            container.main_container.style.marginLeft = '0';
+            side_bar_content.side_bar.style.position = 'sticky';
+        }, 800);
     }
 
     else if (status === "open_menu") {
         side_bar_content.open_menu.style.display = 'none';
         side_bar_content.side_bar.style.width = '250px';
+        if ((window.innerWidth || document.documentElement.clientWidth) < 1260) {
+            side_bar_content.side_bar.style.position = 'absolute';
+            container.main_container.style.marginLeft = '60px';
+        }
+        setTimeout(() => {
+            for (let i = 0; i < side_bar_content.bar_option.length; i++) {
+                side_bar_content.bar_option[i].style.justifyContent = 'start';
+                side_bar_content.bar_option[i].style.paddingLeft = '10%';
+            }
+        }, 300);
         setTimeout(() => {
             side_bar_content.close_menu.style.display = 'flex';
             side_bar_content.logo.style.visibility = 'visible';
@@ -61,37 +96,35 @@ function menu(status = 'close_menu') {
         }, 800);
     }
 }
-}   
 
 
 
 //Side bar Navigation
+
 navigate();
 function navigate(title = "Dashboard") {
-
+    const tab = {
+        show_card: document.querySelectorAll('.show_card'),
+        head_title: document.getElementById('head_title'),
+        opt: document.getElementById(title),
+        card: document.getElementById(title + "_card"),
+        opt_container: document.getElementsByClassName('bar_caontent_elements'),
+    }
+    tab.show_card.forEach(element => {
+        element.style.display = 'none';
+    });
     start_loader();
 
     setTimeout(() => {
         stop_loader();
-        const tab = {
-            show_card: document.querySelectorAll('.show_card'),
-            bar_list: document.querySelectorAll(".bar_options"),
-            head_title: document.getElementById('head_title'),
-            opt: document.getElementById(title),
-            card: document.getElementById(title + "_card"),
-            opt_container: document.querySelector('bar_caontent_elements'),
+        tab.head_title.innerHTML = title;
+        for (let i = 0; i < tab.opt_container.length; i++) {
+            tab.opt_container[i].classList.remove('active');
         }
-        head_title.innerHTML = title;
-        tab.bar_list.forEach(element => {
-            element.classList.remove('active')
-        });
         tab.opt.classList.add('active');
-        // if ((window.innerWidth || document.documentElement.clientWidth) < 1025) {
-        //     menu("close_menu");
-        // }
-        tab.show_card.forEach(element => {
-            element.style.display = "none";
-        });
+        if ((window.innerWidth || document.documentElement.clientWidth) < 1090) {
+            menu("close_menu");
+        }
         tab.card.style.display = "flex";
     }, 1000);
 }
@@ -141,7 +174,6 @@ function open_container() {
 function close_container() {
     outside_container.style.display = 'none';
 
-
     container_contents.icon.innerHTML = "notifications";
     container_contents.container.style.height = '0vh';
     container_contents.container.style.border = 'none';
@@ -150,9 +182,6 @@ function close_container() {
     for (let i = 0; i < container_contents.container_data.length; i++) {
         container_contents.container_data[i].style.display = 'none';
     }
-
-
-
 
     container_contents.icon.removeEventListener("click", close_container);
     container_contents.icon.addEventListener("click", open_container)
